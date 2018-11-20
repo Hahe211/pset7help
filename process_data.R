1c1845f9e829b5d7d8ccd296728e887b1173ad52
Junho Moon <moon@Junhos-MacBook-Pro.local>
2018-11-20 17:53:24 -0500

    Shiny setup

---
+++ process_data.R
@@ -0,0 +1,21 @@
+library(tidyverse)
+library(fs)
+
+# automatically download the file
+download.file(url = "https://goo.gl/ZRCBda",
+              destfile = "master.zip",
+              quiet = TRUE,
+              # making sure the download works whether you have Mac or PC
+              mode = "wb")
+# unzip the downloaded zip file
+unzip("master.zip")
+
+# delete the zip file
+file_delete("master.zip")
+
+# read in the data
+file_names <- dir_ls("2018-live-poll-results-master/data")
+polls <- map_dfr(file_names, read_csv, .id = "source")
+
+results <- read_csv("mt_2_results.csv")
+
